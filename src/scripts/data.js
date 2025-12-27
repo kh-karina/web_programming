4fe110405609c85a88527492d6d9b8dbff1d8a88
Карина Храбуст <karina300502@yandex.ru>
2025-12-28 02:23:36 +0700

    fix images

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -6,7 +6,7 @@ export const products = [
     name: "Корм для собак \"Пушистый»",
     price: 299,
     description: "Натуральный корм с курицей и рисом. Плюшевые хвостики и мягкая шерсть гарантированы!",
-    image: "img/news.svg",
+    image: "https://storage.yandexcloud.net/web-project-khrabust/p1.png",
     category: "собаки"
   },
   /*  2 */
@@ -15,7 +15,7 @@ export const products = [
     name: "Плюшевый мяч для кошек",
     price: 149,
     description: "Мягкий мячик, идеально подходит для игривых кошек. Звончики добавляют звуковой эффект.",
-    image: "img/products/p2.jpg",
+    image: "https://storage.yandexcloud.net/web-project-khrabust/p2.jpg",
     category: "кошки"
   },
   /*  3 */
@@ -24,7 +24,7 @@ export const products = [
     name: "Корм для грызунов «Нутри‑Норм»",
     price: 199,
     description: "Сбалансированный рацион для мышей, хомячков и грызунов. Содержит витамины и минералы.",
-    image: "src/img/products/p3.jpg",
+    image: "https://storage.yandexcloud.net/web-project-khrabust/p3.jpg",
     category: "грызуны"
   },
   /*  4 */
@@ -33,7 +33,7 @@ export const products = [
     name: "Шарик‑игрушка «Резиновый»",
     price: 79,
     description: "Резиновая игрушка с запахом мяса – любимец любой собаки.",
-    image: "src/img/products/p4.jpg",
+    image: "https://storage.yandexcloud.net/web-project-khrabust/p4.jpg",
     category: "собаки"
   },
   /*  5 */
@@ -42,7 +42,7 @@ export const products = [
     name: "Шёлковый шарф «Кошачий»",
     price: 259,
     description: "Лёгкий и мягкий шарф, который будет выглядеть стильно в любых лапках.",
-    image: "img/products/p5.jpg",
+    image: "https://storage.yandexcloud.net/web-project-khrabust/p5.jpg",
     category: "кошки"
   },
   /*  6 */
@@ -51,7 +51,7 @@ export const products = [
     name: "Вакцина «Собаки» 5 шт.",
     price: 349,
     description: "Полный курс вакцинации: дифтерия, парвовирус, бешенство, лептоспироз, парвовирус.",
-    image: "img/products/p6.jpg",
+    image: "https://storage.yandexcloud.net/web-project-khrabust/p6.jpg",
     category: "ветаптека"
   },
   /*  7 */
@@ -60,7 +60,7 @@ export const products = [
     name: "Ветеринарный щипцы «Профи»",
     price: 199,
     description: "Удобные щипцы для чистки ушей и зубов. Антибактериальный покрытие.",
-    image: "img/products/p7.jpg",
+    image: "https://storage.yandexcloud.net/web-project-khrabust/p7.jpg",
     category: "ветаптека"
   },
   /*  8 */
@@ -69,7 +69,7 @@ export const products = [
     name: "Книга «Трудности в питомниках»",
     price: 589,
     description: "Инструкция по уходу за питомцами, от новобранцев до опытных хозяев.",
-    image: "img/products/p8.jpg",
+    image: "https://storage.yandexcloud.net/web-project-khrabust/p8.jpg",
     category: "новости"
   },
   /*  9 */
@@ -78,7 +78,7 @@ export const products = [
     name: "Корм для кошек «Милашка»",
     price: 279,
     description: "Корм из рыбы и курицы. Полноценный рацион для активных кошек.",
-    image: "img/products/p9.jpg",
+    image: "https://storage.yandexcloud.net/web-project-khrabust/p9.jpg",
     category: "кошки"
   },
   /* 10 */
@@ -87,7 +87,7 @@ export const products = [
     name: "Игрушка «Сапфир» – мячик‑песок",
     price: 189,
     description: "Мячик, в котором прячется лакомство. Отлично развивает нюх.",
-    image: "img/products/p10.jpg",
+    image: "https://storage.yandexcloud.net/web-project-khrabust/p10.jpg",
     category: "собаки"
   },
   /* 11 */
@@ -96,7 +96,7 @@ export const products = [
     name: "Ароматический спрей «Лавандовый»",
     price: 129,
     description: "Успокаивает нервную систему животных, уменьшает агрессию.",
-    image: "img/products/p11.jpg",
+    image: "https://storage.yandexcloud.net/web-project-khrabust/p11.jpg",
     category: "ветаптека"
   },
   /* 12 */
@@ -105,7 +105,7 @@ export const products = [
     name: "Корм для грызунов «Пушкин»",
     price: 159,
     description: "Микро‑корм с высоким содержанием клетчатки и витаминов.",
-    image: "img/products/p12.jpg",
+    image: "https://storage.yandexcloud.net/web-project-khrabust/p12.jpg",
     category: "грызуны"
   },
   /* 13 */
@@ -114,7 +114,7 @@ export const products = [
     name: "Покрывало «Облако»",
     price: 359,
     description: "Мягкое, водонепроницаемое покрывало для лежачих мест питомцев.",
-    image: "img/products/p13.jpg",
+    image: "https://storage.yandexcloud.net/web-project-khrabust/p13.jpg",
     category: "собаки"
   },
   /* 14 */
@@ -123,7 +123,7 @@ export const products = [
     name: "Корм для кошек «Кошачья корона»",
     price: 299,
     description: "Натуральный корм с рыбой, курицей и кефиром. Полноценный рацион.",
-    image: "img/products/p14.jpg",
+    image: "https://storage.yandexcloud.net/web-project-khrabust/p14.jpg",
     category: "кошки"
   },
   /* 15 */
@@ -132,7 +132,7 @@ export const products = [
     name: "Кость для собак «Смех»",
     price: 109,
     description: "Кость из натурального каучука, которую собака любит грызть и хранить.",
-    image: "img/products/p15.jpg",
+    image: "https://storage.yandexcloud.net/web-project-khrabust/p15.jpg",
     category: "собаки"
   },
   /* 16 */
@@ -141,7 +141,7 @@ export const products = [
     name: "Набор «Уход» для кошек",
     price: 549,
     description: "Набор: шампунь, щетка, лакомства, когтеточка. Всё, что нужно для домашнего ухода.",
-    image: "img/products/p16.jpg",
+    image: "https://storage.yandexcloud.net/web-project-khrabust/p16.jpg",
     category: "кошки"
   }
 ];
